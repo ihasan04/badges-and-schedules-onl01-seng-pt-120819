b3a7bdd8b938c4b268d3815fee90675119ff2fa1
Ismael Hasan <ismael.hasan@live.com>
2019-12-17 19:46:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 end
 
 def assign_rooms(list_of_speakers)
-    list_of_speakers.each_with_index.map do |name, index| 
+    list_of_speakers.each_with_index do |name, index| 
       "Hello, #{name}! You'll be assigned to room #{index + 1}!"
     end
 end
